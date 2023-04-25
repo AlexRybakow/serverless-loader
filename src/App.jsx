5a8fe67577a88a4e8972eba79267c8c7c579c3ef
AlexRybakow <alex.rybakow725@gmail.com>
2023-04-25 15:59:19 +0300

    add helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import { Main } from './components/Main/PageBody/Page';
 import { Footer } from './components/Footer/Footer';
 import { MobileMenu } from './components/MobileMenu/MobileMenu';
 import { Header } from './components/Header/Header';
-
+import { Helmet } from 'react-helmet'
 
 const App = () => {
   const [menuOpened, setMenuOpened] = useState(false);
@@ -19,12 +19,18 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-    <Header openMenu={handleOpenMenu} />
-    <MobileMenu isMenuOpened={menuOpened} closeMenu={handleCloseMenu} />
-    <Main />
-    <Footer />
-  </div>
+    <>
+      <Helmet>
+        <title>Serverless_Team - Serverless Consulting Services & Development Experts </title>
+        <meta name="description" content="Transform your business with our high-class serverless consulting services. Maximize efficiency and minimize costs. Contact our experts now." />
+      </Helmet>
+      <div className="App">
+        <Header openMenu={handleOpenMenu} />
+        <MobileMenu isMenuOpened={menuOpened} closeMenu={handleCloseMenu} />
+        <Main />
+        <Footer />
+      </div>
+    </>
   );
 }
 
