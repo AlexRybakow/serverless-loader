a6187f81e822fc057ab21b67fe9d5aab1579bd43
AlexRybakow <alex.rybakow725@gmail.com>
2023-04-25 18:23:39 +0300

    spell check

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -14,13 +14,16 @@ export const MobileMenu = ({ isMenuOpened, closeMenu }) => {
       </div>
       <ul className="list">
         <li>
-          <a className="list__item" href="/upwork">Upwork</a>
+        <a href="https://www.upwork.com/ag/serverless/" rel="nofollow" target="_blank" className="header__item">upwork</a>
         </li>
         <li>
-          <a className="list__item" href="/github">Github</a>
+        <a href="https://github.com/ServerlessTeam" rel="nofollow" target="_blank" className="header__item">github</a>
         </li>
         <li>
-          <a className="list__item" href="/linkedin">Linkedin</a>
+        <a href="https://linkedin.com/company/serverless-team" rel="nofollow" target="_blank" className="header__item">linkedin</a>
+        </li>
+        <li>
+        <a href="https://www.instagram.com/serverless.team/" rel="nofollow" target="_blank" className="header__item">instagram</a>
         </li>
       </ul>
     </nav>
