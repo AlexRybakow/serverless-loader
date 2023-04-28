004d60e7bcccf8558cf1b3f11cb075facf7c30d7
Oleksandr Rybakov <sancho725@gmail.com>
2023-04-28 18:28:01 +0300

    typing animation

--- src/components/Main/PageBody/Page.jsx
+++ src/components/Main/PageBody/Page.jsx
@@ -1,5 +1,5 @@
 import './Page.scss';
-import Typed from "react-typed"
+import { TypeAnimation } from 'react-type-animation';
 
 export const Main = () => {
   return (
@@ -9,14 +9,17 @@ export const Main = () => {
       </p>
       <section className="contactUs__container">
        <p>We take care of <span className="contactUs__serverless">
-        <Typed
-      strings={[
-            "serverless",
-          ]}
-          typeSpeed={300}
-          backSpeed={300}
-          loop
-        /></span> <br/> development so you can focus <br/> on your business </p>
+       <TypeAnimation
+      sequence={[
+        'serverless', 
+        2000, 
+        'Serverless', 
+        2000, 
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+    /></span> <br/> development so you can focus <br/> on your business </p>
       </section>
     </section>
   )
