6e1ed4d01fda88f430ff0fbd4146bb46cc8ba144
Oleksandr Rybakov <sancho725@gmail.com>
2023-04-28 17:45:28 +0300

    typing animation

--- src/components/Main/PageBody/Page.jsx
+++ src/components/Main/PageBody/Page.jsx
@@ -1,4 +1,5 @@
 import './Page.scss';
+import Typed from "react-typed"
 
 export const Main = () => {
   return (
@@ -7,7 +8,15 @@ export const Main = () => {
         // serverless.team is loading...
       </p>
       <section className="contactUs__container">
-       <p>We take care of <span className="contactUs__serverless">serverless</span> development so you can focus <br/> on your business </p>
+       <p>We take care of <span className="contactUs__serverless">
+        <Typed
+      strings={[
+            "serverless",
+          ]}
+          typeSpeed={300}
+          backSpeed={300}
+          loop
+        /></span> <br/> development so you can focus <br/> on your business </p>
       </section>
     </section>
   )
